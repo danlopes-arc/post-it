8564476f718d9cd98d6634ff5725386e8cb1c424
Daniel <danlpsarcanjo@gmail.com>
2021-04-20 02:40:47 -0400

    Add cards to explore page

--- src/app/components/pages/explore-page/explore-page.component.ts
+++ src/app/components/pages/explore-page/explore-page.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {DatabaseService} from '../../../services/database.service';
+import {User} from '../../../models/User';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-explore-page',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExplorePageComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+
+  constructor(
+    public router: Router,
+    private database: DatabaseService,
+    private auth: AuthService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  private async loadUsers(): Promise<void> {
+    const user = await this.auth.getUser();
+    if (user) {
+      this.users = await this.database.users.getNonFollowed(user);
+    }
   }
 
 }
