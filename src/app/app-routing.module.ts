8b542b53b663ada38f320979ba07eb78d062f2be
Daniel <danlpsarcanjo@gmail.com>
2021-04-20 16:31:06 -0400

    Implement follow/unfollow logic to profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ const routes: Routes = [
   {path: 'timeline', component: TimelinePageComponent},
   {path: 'about', component: AboutPageComponent},
   {path: 'profile', component: ProfilePageComponent},
+  {path: 'users/:id', component: ProfilePageComponent},
 ];
 
 @NgModule({
--- src/app/components/pages/profile-page/profile-page.component.ts
+++ src/app/components/pages/profile-page/profile-page.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {DatabaseService} from '../../../services/database.service';
 import {AuthService} from '../../../services/auth.service';
 import {User} from '../../../models/User';
@@ -12,12 +12,14 @@ import {User} from '../../../models/User';
 export class ProfilePageComponent implements OnInit {
 
   user: User | null = null;
-  isFollowing = true;
+  authUser: User | null = null;
+  isFollowing = false;
   isAuthUser = false;
 
   constructor(public router: Router,
               private database: DatabaseService,
-              private auth: AuthService) {
+              private auth: AuthService,
+              private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
@@ -25,14 +27,49 @@ export class ProfilePageComponent implements OnInit {
   }
 
   private async loadUser(): Promise<void> {
-    this.user = await this.auth.getUser();
+    const authUser = await this.auth.getUser();
+    this.authUser = authUser;
+    if (!authUser) {
+      this.router.navigate(['']);
+      return;
+    }
+    if (this.activatedRoute.snapshot.routeConfig?.path === 'profile') {
+      this.user = authUser;
+      this.isAuthUser = true;
+      return;
+    }
+    this.user = await this.database.users.findById(this.activatedRoute.snapshot.params.id);
+    if (!this.user) {
+      this.router.navigate(['']);
+      return;
+    }
+    if (this.user.id === authUser.id) {
+      this.isAuthUser = true;
+      return;
+    }
+    const followers = await this.database.users.getFollowers(this.user);
+    if (followers.find(user => user.id === authUser.id)) {
+      this.isFollowing = true;
+    }
   }
 
   onFollow = async (): Promise<void> => {
-    console.log('follow');
+    if (!this.authUser || !this.user) {
+      return;
+    }
+    await this.database.users.addFollower(this.authUser, this.user);
+    this.isFollowing = true;
   }
 
   onUnfollow = async (): Promise<void> => {
-    console.log('follow');
+    if (!this.authUser || !this.user) {
+      return;
+    }
+    await this.database.users.removeFollower(this.authUser, this.user);
+    this.isFollowing = false;
+  }
+
+  onEdit = async (): Promise<void> => {
+    console.log('edit');
   }
 }
