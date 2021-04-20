f8756b121b29a07bb8c8228291e89b9746f88471
Daniel <danlpsarcanjo@gmail.com>
2021-04-20 13:40:58 -0400

    Create user-card with static data and add it to timeline page

--- src/app/components/pages/timeline-page/timeline-page.component.ts
+++ src/app/components/pages/timeline-page/timeline-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {DatabaseService} from '../../../services/database.service';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-timeline-page',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelinePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    private database: DatabaseService,
+    private auth: AuthService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  private async loadUsers(): Promise<void> {
+    const user = await this.auth.getUser();
+    if (user) {
+      const users = await this.database.users.getFollowed(user);
+    }
   }
 
 }
