7870eecc56d2f680748ce9823ffa677e19d51f48
Daniel <danlpsarcanjo@gmail.com>
2021-04-21 00:25:04 -0400

    Make post-card dynamic

--- src/app/components/parts/post-card/post-card.component.ts
+++ src/app/components/parts/post-card/post-card.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Post} from '../../../models/Post';
+import {relativeTime} from 'human-date';
+import {Router} from '@angular/router';
+import {DatabaseService} from '../../../services/database.service';
+import {User} from '../../../models/User';
 
 @Component({
   selector: 'app-post-card',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostCardComponent implements OnInit {
 
-  constructor() { }
+  relativeTime = relativeTime;
+  user: User | null = null;
 
-  ngOnInit(): void {
+  @Input() post: Post | null = null;
+
+  constructor(public router: Router,
+              private database: DatabaseService) {
+  }
+
+  async ngOnInit(): Promise<void> {
+    if (!this.post) {
+      return;
+    }
+    this.user = await this.database.users.getPostUser(this.post);
   }
 
 }
