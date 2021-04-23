08dc19337e09e0de9adf7e8cd44274bd4bab0db1
Daniel <danlpsarcanjo@gmail.com>
2021-04-22 22:11:08 -0400

    Add support to editing location to edit post

--- src/app/components/pages/edit-post-page/edit-post-page.component.ts
+++ src/app/components/pages/edit-post-page/edit-post-page.component.ts
@@ -5,6 +5,7 @@ import {AuthService} from '../../../services/auth.service';
 import {DatabaseService} from '../../../services/database.service';
 import {Location} from '@angular/common';
 import {Post} from '../../../models/Post';
+import {getCoordinatesAddress, getCurrentLocation} from '../../../utils/geolocation';
 
 @Component({
   selector: 'app-edit-post-page',
@@ -24,11 +25,16 @@ export class EditPostPageComponent implements OnInit {
       Validators.minLength(1),
       Validators.maxLength(300)
     ]],
+    saveLocation: [false, []],
   });
   post: Post | null = null;
   isLoading = false;
   isSubmitted = false;
   shouldDhowDeleteConfirmation = false;
+
+  address: string | null = null;
+  latitude: number | null = null;
+  longitude: number | null = null;
 
   constructor(public router: Router,
               private auth: AuthService,
@@ -56,6 +62,14 @@ export class EditPostPageComponent implements OnInit {
 
     this.form.get('title')?.markAsTouched();
     this.form.get('body')?.markAsTouched();
+
+    this.latitude = this.post.latitude;
+    this.longitude = this.post.longitude;
+
+    if (this.post.hasCoordinates()) {
+      this.form.get('saveLocation')?.setValue(true);
+      this.address = await getCoordinatesAddress(this.latitude || 0, this.longitude || 0);
+    }
   }
 
   getErrors(controlName: string): string[] {
@@ -78,6 +92,13 @@ export class EditPostPageComponent implements OnInit {
 
     this.post.title = this.form.get('title')?.value || '';
     this.post.body = this.form.get('body')?.value || '';
+    if (this.address) {
+      this.post.latitude = this.latitude;
+      this.post.longitude = this.longitude;
+    } else {
+      this.post.latitude = null;
+      this.post.longitude = null;
+    }
 
     await this.database.posts.update(this.post);
     await this.router.navigate(['posts', this.post?.id]);
@@ -116,5 +137,22 @@ export class EditPostPageComponent implements OnInit {
 
   onDeleteCancel = (): void => {
     this.shouldDhowDeleteConfirmation = false;
+  }
+
+  async onSaveLocationSelected(): Promise<void> {
+    if (!this.form.get('saveLocation')?.value) {
+      const resp = await getCurrentLocation();
+      if (!resp.location) {
+        this.form.get('saveLocation')?.setValue(false);
+      } else {
+        this.latitude = resp.location.coords.latitude;
+        this.longitude = resp.location.coords.longitude;
+        this.address = await getCoordinatesAddress(this.latitude || 0, this.longitude || 0);
+        return;
+      }
+    }
+    this.latitude = null;
+    this.longitude = null;
+    this.address = null;
   }
 }
