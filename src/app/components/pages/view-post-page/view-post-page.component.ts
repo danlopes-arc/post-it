22c1f5256e2f09c1ddec18a71610e3d7a63c5c56
Daniel <danlpsarcanjo@gmail.com>
2021-04-23 01:52:59 -0400

    Show map on view-post page (incomplete)

--- src/app/components/pages/view-post-page/view-post-page.component.ts
+++ src/app/components/pages/view-post-page/view-post-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Post} from '../../../models/Post';
 import {User} from '../../../models/User';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -24,6 +24,7 @@ export class ViewPostPageComponent implements OnInit {
   userComments: Comment[] = [];
   commentators: User[] = [];
   address: string | null = null;
+  @ViewChild('map') map: ElementRef | null = null;
 
   constructor(public router: Router,
               private database: DatabaseService,
@@ -46,6 +47,7 @@ export class ViewPostPageComponent implements OnInit {
     }
     if (this.post.hasCoordinates()) {
       this.address = await getCoordinatesAddress(this.post.latitude || 0, this.post.longitude || 0);
+      this.loadMap();
     }
     this.authUser = await this.auth.getUser();
     this.postUser = await this.database.users.getPostUser(this.post);
@@ -59,4 +61,41 @@ export class ViewPostPageComponent implements OnInit {
       }
     }
   }
+
+  async loadMap(): Promise<void> {
+    const mapContainer = document.getElementById('map');
+    if (!mapContainer) {
+      return;
+    }
+    const coords = {
+      lat: this.post?.latitude || 0,
+      lng: this.post?.longitude || 0
+    };
+    const platform = new H.service.Platform({
+      apikey: 'phG3bW_BRgRzfbyc_k8LlWDhKyDqE_VYEPfMX4fApPA'
+    });
+    const defaultLayers = platform.createDefaultLayers();
+    // Instantiate (and display) a map object:
+    const map = new H.Map(
+      mapContainer,
+      defaultLayers.vector.normal.map,
+      {
+        zoom: 10,
+        center: coords,
+        pixelRatio: window.devicePixelRatio || 1
+      });
+    window.addEventListener('resize', () => map.getViewPort().resize());
+    const ui = H.ui.UI.createDefault(map, defaultLayers);
+    ui.getControl('mapsettings').setVisibility(false);
+    const bubble = new H.ui.InfoBubble(coords, {
+      content: this.address || ''
+    });
+    // tslint:disable-next-line:no-unused-expression
+    new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    // ui.addBubble(bubble);
+    const icon = new H.map.Icon('assets/marker.svg');
+    const marker = new H.map.Marker(coords, {icon});
+    map.addObject(marker);
+  }
+
 }
